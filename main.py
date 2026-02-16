d68d6b84b72aeeeee120d3d1fa4ecb085aea7d29
cala <magebane4@gmail.com>
2026-02-16 18:21:42 -0500

    Asteroid Fields Complete

--- main.py
+++ main.py
@@ -2,21 +2,36 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # update player
+        updatable.update(dt)
+        # rendering
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        # calculating dt
         dt = clock.tick(60)/1000
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
@@ -24,3 +39,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# ----------------------------------------
+# Boot.dev Reminder:
+# Activate venv each new terminal:
+# cd ~/bootdev/asteroids
+# source .venv/bin/activate
+# ----------------------------------------
